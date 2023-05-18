48e735dbb61531cf2844a05558bc6c9d56c1b74c
d.tkakishak <Tkakishakd@mail.ru>
2023-05-18 13:25:10 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.*;
+
 public class Main {
+    private  static  final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private  static  final List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+    public static void task1(){
+        System.out.println("Задача №1");
+        for (int num : nums) {
+            if (num % 2 !=0){
+                System.out.println(num);
+            }
+        }
+    }
+    public static void task2(){
+        System.out.println("Задача №2");
+        Collections.sort(nums);
+
+        int prevNum = Integer.MIN_VALUE;
+        for (Integer num : nums) {
+            if (num % 2 == 0 && num != prevNum){
+                System.out.println(num);
+                prevNum = num;
+            }
+
+        }
+    }
+    public static void task3(){
+        System.out.println("Задача №3");
+        Set<String> uniqWords = new HashSet<>(strings);
+        System.out.println(uniqWords);
+    }
+
+    public static void task4(){
+        System.out.println("Задача №4");
+        Set<String> uniqWords = new HashSet<>(strings);
+        System.out.println(strings.size()- uniqWords.size());
+
     }
 }
